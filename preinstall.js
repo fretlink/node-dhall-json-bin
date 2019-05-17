03aaa7d3bbf9ff2176fe3b69138f37f5f265d186
Cyril Sobierajewicz <cyril.sobierajewicz@fretlink.com>
2019-05-17 23:59:04 +0200

    Add Windows support

--- preinstall.js
+++ preinstall.js
@@ -1,10 +1,15 @@
 "use strict";
 
+const fs = require("fs");
+const path = require("path");
+
 const { https } = require("follow-redirects");
 const tar = require("tar");
 const unbz2 = require("unbzip2-stream");
+const unzipper = require("unzipper");
 
 const pkg = require("./package.json");
+const bindir = path.join(__dirname, "bin");
 
 const trim = str => str && String(str).trim();
 
@@ -16,8 +21,29 @@ if (!dhallJsonVersion) throw new Error("Missing DHALL_JSON_VERSION environment v
 
 const release = `https://github.com/dhall-lang/dhall-haskell/releases/download/${dhallVersion}/dhall-json-${dhallJsonVersion}`;
 
-const url = `${release}-x86_64-linux.tar.bz2`;
-https.get(url, res => {
-  if (res.statusCode >= 400) throw new Error(`Error fetching ${url}: ${res.statusMessage}`);
-  res.pipe(unbz2()).pipe(tar.x({ C: __dirname }));
-});
+const get = (archive, callback) => {
+  const url = `${release}-${archive}`;
+  return https.get(url, res => {
+    if (res.statusCode >= 400) throw new Error(`Error fetching ${url}: ${res.statusMessage}`);
+    return callback(res);
+  });
+};
+
+if (process.platform === "win32") {
+  get("x86_64-windows.zip", res =>
+    res.pipe(unzipper.Extract({ path: bindir }))
+  );
+} else {
+  get("x86_64-linux.tar.bz2", res =>
+    res.pipe(unbz2()).pipe(tar.x({ C: __dirname }).on("finish", () => {
+      fs.readdir(bindir, (err, names) => {
+        if (err) throw err;
+        for (const name of names) {
+          fs.rename(path.join(bindir, name), path.join(bindir, name + ".exe"), err => {
+            if (err) throw err;
+          });
+        }
+      });
+    }))
+  );
+}
